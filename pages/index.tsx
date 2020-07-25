b40b2be156f79123617b8ffa345c439f96c1dfd3
gabriel garcia <gabriel@streamloots.com>
2020-07-25 09:58:47 +0200

    parse nodes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,140 @@
 import * as React from 'react';
 import { ControlledEditor } from '@monaco-editor/react';
+import * as acorn from 'acorn';
+import * as walk from 'acorn-walk';
+import prettier from 'prettier/standalone';
+import parserBabel from 'prettier/parser-babel';
 
 import { Grid } from '@components/grid';
+
+const formatJs = (code) => {
+  return prettier.format(code, { parser: 'babel', plugins: [parserBabel] });
+};
+
+const getValueEditor = () => {
+  return formatJs(`
+/* 
+I accept an object like
+const PROPS = {
+  message: 'hello this is my message',
+  count: 5,
+} 
+*/`);
+};
+
+const commonTemplate = `
+import React from 'react';
+import { text, number } from '@storybook/addon-knobs';
+import { Component } from './index';
+
+export default {
+  title: 'Component',
+  component: Component,
+};
+`;
+
+const parseJavascriptToArrayNodes = (code) => {
+  const parsedJS = acorn.parse(code);
+
+  let nodes = [];
+
+  walk.simple(parsedJS, {
+    ObjectExpression(node: any) {
+      nodes.push(node.properties);
+    }
+  });
+
+  nodes = JSON.parse(JSON.stringify(nodes));
+
+  nodes = nodes[0];
+
+  const parsedNodes = nodes.map((node) => {
+    return {
+      key: node.key.name,
+      value: node.value.value,
+      type: typeof node.value.value
+    };
+  });
+
+  return parsedNodes;
+};
+
+const getTypeFn = (type) => {
+  if (type === 'number') {
+    return 'number';
+  }
+
+  return 'text';
+};
+
+const getValue = (value, type) => {
+  if (type === 'string') {
+    return `"${value}"`;
+  }
+
+  return value;
+};
+
+const generateStorybookFields = (nodes) => {
+  const parsedString = nodes.map((node) => {
+    return `const ${node.key} = ${getTypeFn(node.type)}('${
+      node.key
+    }', ${getValue(node.value, node.type)});\n`;
+  });
+
+  return parsedString.join('');
+};
+
+const generateComponentRendering = (nodes) => {
+  return nodes
+    .map((node) => {
+      return `${node.key}={${node.key}}\t`;
+    })
+    .join('');
+};
+
+const generateStorybook = (nodes) => {
+  return `
+    ${commonTemplate}
+
+    export const ComponentDemo = () => {
+      ${generateStorybookFields(nodes)}
+      return (
+        <Component ${generateComponentRendering(nodes)} />
+      )
+     
+    }
+  `;
+};
 
 //eslint-disable-next-line
 export default function Home() {
   const [isEditorReady, setIsEditorReady] = React.useState(false);
-  const [value, setValue] = React.useState('');
-  const valueGetter = React.useRef<() => void>();
+  const [value, setValue] = React.useState(getValueEditor());
+  const [outputValue, setOutputValue] = React.useState('');
 
-  function handleEditorDidMount(_valueGetter) {
+  function handleEditorDidMount() {
     setIsEditorReady(true);
-    valueGetter.current = _valueGetter;
   }
 
   const handleEditorChange = (ev, value) => {
     setValue(value);
   };
 
+  const parseJS = () => {
+    const parsedNodes = parseJavascriptToArrayNodes(value);
+
+    const generatedStorybookComponent = formatJs(
+      generateStorybook(parsedNodes)
+    );
+
+    setOutputValue(generatedStorybookComponent);
+  };
+
   return (
     <React.Fragment>
-      <button
-        onClick={() => console.log('value', value)}
-        disabled={!isEditorReady}
-      >
-        Show value
+      <button onClick={parseJS} disabled={!isEditorReady}>
+        Parse value
       </button>
       <Grid>
         <Grid.Column col={6}>
@@ -42,8 +152,7 @@ export default function Home() {
             theme="dark"
             height="100vh"
             language="javascript"
-            value={value}
-            onChange={handleEditorChange}
+            value={outputValue}
             editorDidMount={handleEditorDidMount}
             options={{ readOnly: true }}
           />
